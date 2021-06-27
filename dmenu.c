07314a14de7d09c1c308ee36db5bde3d01e62ed1
Victor Timofei <victor@vtimothy.com>
2021-06-27 16:06:27 +0300

    Change dmenu version

--- dmenu.c
+++ dmenu.c
@@ -846,7 +846,7 @@ main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 		/* these options take no arguments */
 		if (!strcmp(argv[i], "-v")) {      /* prints version information */
-			puts("dmenu-"VERSION);
+			puts("dmenu-vtimofei");
 			exit(0);
 		} else if (!strcmp(argv[i], "-b")) /* appears at the bottom of the screen */
 			topbar = 0;
